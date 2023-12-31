dfeb94c1ba62a3937c7c94e1d3c13a8f5d2f2abe
milan <lukhimilan123@gmail.com>
2023-12-31 10:13:12 +0530

    add try catch

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -6,62 +6,67 @@ const converter = require("number-to-words");
 const moment = require("moment");
 
 async function downloadFile(req, res) {
-  console.log("req", req.body);
-  const customer = await Customers.findOne({ _id: req.body.firmName });
-  let data = req.body;
+  try {
+    console.log("req", req.body);
+    const customer = await Customers.findOne({ _id: req.body.firmName });
+    let data = req.body;
 
-  data.date = moment(data.date).format("DD/MM/YYYY");
-  data.total = parseFloat(data.total).toFixed(2);
-  data.cgst = parseFloat(data.cgst).toFixed(2);
-  data.sgst = parseFloat(data.sgst).toFixed(2);
-  data.subTotal = parseFloat(data.subTotal).toFixed(2);
-  data.rate = parseFloat(data.rate).toFixed(2);
+    data.date = moment(data.date).format("DD/MM/YYYY");
+    data.total = parseFloat(data.total).toFixed(2);
+    data.cgst = parseFloat(data.cgst).toFixed(2);
+    data.sgst = parseFloat(data.sgst).toFixed(2);
+    data.subTotal = parseFloat(data.subTotal).toFixed(2);
+    data.rate = parseFloat(data.rate).toFixed(2);
 
-  let gstInPer = parseFloat(data.gstPercentage / 2).toFixed(2);
-  let totalWords = converter.toWords(data.total);
-  let word = Number(data.cgst) + Number(data.sgst);
-  console.log("word, word", word);
-  let gstWords = converter.toWords(word);
+    let gstInPer = parseFloat(data.gstPercentage / 2).toFixed(2);
+    let totalWords = converter.toWords(data.total);
+    let word = Number(data.cgst) + Number(data.sgst);
+    console.log("word, word", word);
+    let gstWords = converter.toWords(word);
 
-  const options = { format: "A4" };
+    const options = { format: "A4" };
 
-  const filepath = path.join(__dirname, "../helpers/templates/invoice.ejs");
-  let html = await ejs.renderFile(filepath, {
-    data,
-    customer,
-    gstWords,
-    totalWords,
-    gstInPer,
-  });
+    const filepath = path.join(__dirname, "../helpers/templates/invoice.ejs");
+    let html = await ejs.renderFile(filepath, {
+      data,
+      customer,
+      gstWords,
+      totalWords,
+      gstInPer,
+    });
 
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.setContent(html);
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.setContent(html);
 
-  const pdfBuffer = await page.pdf({
-    format: "A4", // Page format
-  });
+    const pdfBuffer = await page.pdf({
+      format: "A4", // Page format
+    });
 
-  await browser.close();
+    await browser.close();
 
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "Content-Disposition",
-    `attachment; filename=${customer.firmName}-${data.date}.pdf`
-  );
-  res.send(pdfBuffer);
-  // pdf.create(html, options).toBuffer((err, buffer) => {
-  //   if (err) return console.error(err);
-  //   res.setHeader("Content-Type", "application/pdf");
-  //   res.setHeader(
-  //     "Content-Disposition",
-  //     `attachment; filename=${customer.firmName}-${data.date}.pdf`
-  //   );
-  //   res.send(buffer);
-  // });
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${customer.firmName}-${data.date}.pdf`
+    );
+    res.send(pdfBuffer);
+    // pdf.create(html, options).toBuffer((err, buffer) => {
+    //   if (err) return console.error(err);
+    //   res.setHeader("Content-Type", "application/pdf");
+    //   res.setHeader(
+    //     "Content-Disposition",
+    //     `attachment; filename=${customer.firmName}-${data.date}.pdf`
+    //   );
+    //   res.send(buffer);
+    // });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong", error: error });
+  }
 }
 
 module.exports = {

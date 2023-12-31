9d3a1fd4f906f79a3056492b0e68b05f58c02ff1
milan <lukhimilan123@gmail.com>
2023-12-31 11:14:11 +0530

    add middleware

--- src/routes/pdfRoute.js
+++ src/routes/pdfRoute.js
@@ -1,7 +1,7 @@
 const { downloadFile } = require("../controllers/pdfController");
-
+const hasApiAccess = require("../config/middleware");
 const route = require("express").Router();
 
-route.post("/generateBill", downloadFile);
+route.post("/generateBill", hasApiAccess, downloadFile);
 
 module.exports = route;
